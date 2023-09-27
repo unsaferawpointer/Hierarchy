a9129d89453c920f9759cfade2d30fba9c195e75
Anton Cherkasov <unsaferawpointer@gmail.com>
2023-09-27 19:24:23 +0300

    Initial Commit

---
+++ Hierarchy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hierarchy
+//
+//  Created by Anton Cherkasov on 27.09.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
